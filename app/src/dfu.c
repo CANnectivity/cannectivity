2815adcb5d7506bc6e2ab96a286ee06c92de215b
Henrik Brix Andersen <henrik@brixandersen.dk>
2024-10-16 14:53:19 +0000

    app: turn off DFU LED once booted
    Turn off the mcuboot-led0 alias LED upon booting. This optional LED will be
    turned on by MCUboot when entering USB DFU mode, but it is not turned off
    again by MCUboot.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

--- app/src/dfu.c
+++ app/src/dfu.c
@@ -5,11 +5,35 @@
  */
 
 #include <zephyr/dfu/mcuboot.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 #include "cannectivity.h"
 
 LOG_MODULE_REGISTER(dfu, CONFIG_CANNECTIVITY_LOG_LEVEL);
+
+#define DFU_LED_NODE DT_ALIAS(mcuboot_led0)
+
+#ifdef CONFIG_CANNECTIVITY_DFU_LED
+static int dfu_led_init(void)
+{
+	struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DFU_LED_NODE, gpios);
+	int err;
+
+	if (!gpio_is_ready_dt(&led)) {
+		LOG_ERR("DFU LED device not ready");
+		return -ENODEV;
+	}
+
+	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	if (err != 0) {
+		LOG_ERR("failed to turn off DFU LED (err %d)", err);
+		return err;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_CANNECTIVITY_DFU_LED */
 
 int cannectivity_dfu_init(void)
 {
@@ -29,6 +53,13 @@ int cannectivity_dfu_init(void)
 
 		LOG_INF("image confirmed");
 	}
+
+#ifdef CONFIG_CANNECTIVITY_DFU_LED
+	err = dfu_led_init();
+	if (err != 0) {
+		return err;
+	}
+#endif /* CONFIG_CANNECTIVITY_DFU_LED */
 
 	return 0;
 }
